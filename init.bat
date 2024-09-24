07a3a968959e84b73245a6853e8aec6012804581
panda8246 <2804937582@qq.com>
2024-09-24 15:30:21 +0800

    init

---
+++ init.bat
@@ -0,0 +1,5 @@
+py -3 -m venv .venv || exit /b
+CALL .\.venv\Scripts\activate || exit /b
+python -m ensurepip
+python -m pip install --upgrade pip
+python -m pip install -e ".[dev]" || exit /b
---
+++ lab1/test_lib.py
@@ -0,0 +1,8 @@
+from scipy.spatial.transform import Rotation as R
+
+a = R.from_rotvec([0, 3, 2])
+b = R.from_rotvec([2, 1, 5])
+c = R.from_rotvec([4, 7, 3])
+ans1 = (a * b * c).as_matrix()
+ans2 = a.as_matrix() @ b.as_matrix() @ c.as_matrix()
+print(ans1, ans2)
